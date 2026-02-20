a54498b027f700fa4a5bc96a5ea468a306cff26f
Livia Cechova <liviacechova@Liv72---MacBook-Pro.local>
2026-02-20 14:37:38 +0200

    Auth: Fix syntax error in middleware

--- independent-app/src/utils/supabase/middleware.ts
+++ independent-app/src/utils/supabase/middleware.ts
@@ -90,18 +90,3 @@ export async function updateSession(request: NextRequest) {
 
     return supabaseResponse
 }
-
-// IMPORTANT: You *must* return the supabaseResponse object as is. If you're creating a
-// new response object with NextResponse.next() make sure to:
-// 1. Pass the request in it, like so:
-//    const myNewResponse = NextResponse.next({ request })
-// 2. Copy over the cookies, like so:
-//    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
-// 3. Change the myNewResponse object to fit your needs, but avoid modifying
-//    the cookies!
-// Keep in mind that this should only be used in specific situations, like for examples:
-// - Setting HTTP-only cookies
-// - Redirecting the user to a new page but still keeping the session active
-
-return supabaseResponse
-}
