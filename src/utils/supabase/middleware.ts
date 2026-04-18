aed55892d47e5c71d78d1cd3f699d9849fe7a294
Ian <208441554+ian777281@users.noreply.github.com>
2026-04-18 17:26:28 +0200

    revert httpOnly due to supa restriction

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -16,14 +16,7 @@ export async function updateSession(request: NextRequest) {
                 setAll(cookiesToSet) {
                     cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
                     supabaseResponse = NextResponse.next({ request })
-                    cookiesToSet.forEach(({ name, value, options }) =>
-                        supabaseResponse.cookies.set(name, value, {
-                            ...options,
-                            httpOnly: true,
-                            secure: process.env.NODE_ENV === 'production',
-                            sameSite: 'lax',
-                        })
-                    )
+                    cookiesToSet.forEach(({ name, value, options }) => supabaseResponse.cookies.set(name, value, options))
                 },
             },
         }
